e2123328d135cf87171c6900da8865f8dad45aee
justini9 <justini9@students.cs.ubc.ca>
2024-01-17 14:49:54 -0800

    updated projects

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -9,54 +9,6 @@ function About() {
             <div className="pt-4 pl-12">
               <div className="inline-block">
                 <a
-                  href="mailto:justin.ishiguro@gmail.com?subject=Reddit Bot Code Request&body=*This project is still under construction, so I have not uploaded the code to GitHub. If you would like to access the code right now, please feel free to email.*%0A%0A"
-                  target="_blank"
-                  className="col-span-1 svg-link"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="2em"
-                    viewBox="0 0 496 512"
-                  >
-                    <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
-                  </svg>
-                </a>
-              </div>
-            </div>
-            <h1 className="col-span-1 font-bold pt-4 text-md sm:text-2xl hover:text-gray-500 font-serif text-center">
-              Engineering Specialization Reddit Bot
-            </h1>
-            <p className="col-span-1 pt-4 text-right font-bold pr-12 text-xs sm:text-base">
-              June 2023 - Present
-            </p>
-          </div>
-          <p className="px-12 pt-4">
-            As a personal project, I am creating a Reddit bot that will allow
-            users to gather the averages of student's and what specialization
-            they got into on the{" "}
-            <a
-              className="underline"
-              target="_blank"
-              href="https://www.reddit.com/r/ubcengineering/"
-            >
-              r/ubcengineering
-            </a>{" "}
-            page. Students are always trying to see if their first year average
-            is sufficient to get into the specialization of their choice, so I
-            wanted to make an easy way to collect the averages of past students
-            who were accepted to the specialization in previous years.
-          </p>
-        </div>
-        <div className="flex flex-wrap text-sm whitespace-nowrap mx-12">
-          <div className="border-[1px] border-blue-200 bg-blue-200 text-blue-800 rounded-2xl mr-2 mt-2 inline-block px-2">
-            Python
-          </div>
-        </div>
-        <div className="flex flex-col items-center">
-          <div className="grid grid-cols-3 w-full">
-            <div className="pt-8 pl-12">
-              <div className="inline-block">
-                <a
                   href="https://github.com/justinishiguro/JustinIshiguro.com"
                   target="_blank"
                   className="col-span-1 svg-link"
@@ -118,7 +70,61 @@ function About() {
             React Router
           </div>
         </div>
-
+        <div className="flex flex-col items-center">
+          <div className="grid grid-cols-3 w-full">
+            <div className="pt-8 pl-12">
+              <div className="inline-block">
+                <a
+                  href="mailto:justin.ishiguro@gmail.com?subject=Database Project Code Request&body=*Since this is an academic project, I am unable to publicly upload the code to GitHub. Please email to request access to the source code.*%0A%0A"
+                  target="_blank"
+                  className="col-span-1 svg-link"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="2em"
+                    viewBox="0 0 496 512"
+                  >
+                    <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <h1 className="col-span-1 font-bold pt-4 text-md sm:text-2xl hover:text-gray-500 font-serif text-center">
+              Muliplayer Video Game Database Project
+            </h1>
+            <p className="col-span-1 pt-4 text-right font-bold pr-12 text-xs sm:text-base">
+              September 2023 - December 2023
+            </p>
+          </div>
+          <p className="px-12 pt-4">
+            As part of my Relational Database class (CPSC 304), I worked with two 
+            other students to create an application using Java and Oracle to allow
+            users to view the current state of a hypothetical multiplayer video game. 
+            We began our project by creating a relational schema to plan the various 
+            entities and attributes, including players, items, quests, and many others. 
+            Then we began to write our SQL statements using SELECT, UPDATE, and INSERT
+            operations. We also had to implement SQL functionalities such as Selection,
+            Projection, Join, Group By, and Division to allow users to view and edit various 
+            attributes.
+            <br />
+            <br />
+            <span className="font-bold">
+              What I learned from this project:{" "}
+            </span>
+            From this database project, I was able to apply what I learned in class
+            to create a functioning application. Being able to write the SQL statements
+            for our specific project solidified my understanding of how relational 
+            databases work and how users can interact with them through SQL.  
+          </p>
+        </div>
+        <div className="flex flex-wrap text-sm whitespace-nowrap mx-12">
+          <div className="border-[1px] border-blue-200 bg-blue-200 text-blue-800 rounded-2xl mr-2 mt-2 inline-block px-2">
+            SQL
+          </div>
+          <div className="border-[1px] border-blue-200 bg-blue-200 text-blue-800 rounded-2xl mr-2 mt-2 inline-block px-2">
+            Java
+          </div>
+        </div>
         <div className="flex flex-col items-center">
           <div className="grid grid-cols-3 w-full">
             <div className="pt-8 pl-12">
